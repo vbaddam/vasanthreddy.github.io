fff71ab047fc508a6ee7f6120216651df5775bbf
Vasanth Reddy Baddam <vasanth@macbook-pro.myfiosgateway.com>
2021-09-11 01:50:58 -0400

    updated the website

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <nav className="nav-style">
+      <div>Home</div>
+      <div>About Publications</div>
+    </nav>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,23 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+import Header from "./sections/header";
+import About from "./sections/about";
+import Info from "./sections/info";
+import News from "./sections/news";
+import AutoScale from "react-auto-scale"
+
+document.body.style.backgroundColor = "black";
+
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <div>
+    <Header />
+    <About />
+    <Info />
+    <News />
+  </div>,
+  rootElement
+);
